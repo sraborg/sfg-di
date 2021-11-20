10f1ad9a9cb9c1994fe4d55cf8123002d169c741
Sean Raborg <sean.raborg@gmail.com>
2021-11-19 21:40:59 -0700

    Fixed Issue by adding missing POM & Maven Files

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -7,62 +7,62 @@ import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
 import org.springframework.context.annotation.*;
 
-//@ImportResource("classpath:sfgdi-config.xml")
+@ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
 
     @Bean
-    PetServiceFactory petServiceFactory() {
+    PetServiceFactory petServiceFactory(){
         return new PetServiceFactory();
     }
 
     @Profile({"dog", "default"})
     @Bean
-    PetService dogPetService(PetServiceFactory petServiceFactory) {
-        return petServiceFactory.getPetService("dog");
+    PetService dogPetService(PetServiceFactory petServiceFactory){
+       return petServiceFactory.getPetService("dog");
     }
 
-    @Profile("cat")
     @Bean
-    PetService catPetService(PetServiceFactory petServiceFactory) {
+    @Profile("cat")
+    PetService catPetService(PetServiceFactory petServiceFactory){
         return petServiceFactory.getPetService("cat");
     }
 
     @Profile({"ES", "default"})
     @Bean("i18nService")
-    I18NSpanishService i18NSpanishService() {
+    I18NSpanishService i18NSpanishService(){
         return new I18NSpanishService();
     }
 
     @Bean
-    EnglishGreetingRepository englishGreetingRepository() {
+    EnglishGreetingRepository englishGreetingRepository(){
         return new EnglishGreetingRepositoryImpl();
     }
 
     @Profile("EN")
     @Bean
-    I18nEnglishGreetingService i18nService(EnglishGreetingRepository englishGreetingRepository) {
+    I18nEnglishGreetingService i18nService(EnglishGreetingRepository englishGreetingRepository){
         return new I18nEnglishGreetingService(englishGreetingRepository);
     }
 
     @Primary
     @Bean
-    PrimaryGreetingService primaryGreetingService() {
+    PrimaryGreetingService primaryGreetingService(){
         return new PrimaryGreetingService();
     }
 
-    @Bean
-    ConstructorGreetingService constructorGreetingService() {
-        return new ConstructorGreetingService();
-    }
+//    @Bean
+//    ConstructorGreetingService constructorGreetingService() {
+//        return new ConstructorGreetingService();
+//    }
 
     @Bean
-    PropertyInjectedGreetingService propertyInjectedGreetingService() {
+    PropertyInjectedGreetingService propertyInjectedGreetingService(){
         return new PropertyInjectedGreetingService();
     }
 
     @Bean
-    SetterInjectedGreetingService setterInjectedGreetingService() {
+    SetterInjectedGreetingService setterInjectedGreetingService(){
         return new SetterInjectedGreetingService();
     }
 }
