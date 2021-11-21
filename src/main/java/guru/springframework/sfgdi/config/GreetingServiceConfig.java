dba4669d9836f96745f9b06aa78549de4927acf1
Sean Raborg <sean.raborg@gmail.com>
2021-11-20 17:34:12 -0700

    Fixed an error with a missing )

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.*;
 public class GreetingServiceConfig {
 
     @Bean
-    FakeDataSource fakeDataSource(SfgConfiguration sfgConfiguration {
+    FakeDataSource fakeDataSource(SfgConfiguration sfgConfiguration) {
 
         FakeDataSource fakeDataSource = new FakeDataSource();
         fakeDataSource.setUsername(sfgConfiguration.getUsername());
